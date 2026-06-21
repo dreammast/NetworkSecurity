6039f01069fa347435efa945307f76e5cac8f5c0
dreammast <dreammasterorigin@gmail.com>
2026-06-21 21:15:29 +0530

    end commit

--- network_security/components/model_trainer.py
+++ network_security/components/model_trainer.py
@@ -17,7 +17,8 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier,
 )
-
+import dagshub
+dagshub.init(repo_owner='dreammast', repo_name='NetworkSecurity', mlflow=True)
 
 class ModelTrainer:
     def __init__(self,model_trainer_config:ModelTrainerConfig,data_transformation_artifact:DataTransformationArtifact):
@@ -98,6 +99,8 @@ class ModelTrainer:
 
         Network_model  = NetworkModel(preprocessor=preprocessor,model = best_model)
         save_object(self.model_trainer_config.trained_model_file_path,obj=NetworkModel)
+        save_object("final_models/model.pkl",best_model)
+
         ModelTrainerArtifact(trained_model_file_path=self.model_trainer_config.trained_model_file_path,
                              train_metric_artifact=classification_train_metric,
                              test_metric_artifact=classfication_test_metric)
